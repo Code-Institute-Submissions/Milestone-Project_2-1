633641caf88120ec19ae8558de9ac918d449e97d
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-03-04 15:03:28 +0000

    Adding further code to index.js, attempting to enclose data request within search function

--- index.js
+++ index.js
@@ -1,21 +1,33 @@
 
+function displayData() {
+
+    
+}
 
 
+function search(){
 
-let xhr = new XMLHttpRequest();
-let APIKEY = "b010fe05a02c4ddc8336e4c77243bb3c";
-let query = "competitions?areas=2072";
+    var user_input = document.getElementById("user_input").value;
+    console.log(user_input);
+
+
+var xhr = new XMLHttpRequest();
+var APIKEY = "b010fe05a02c4ddc8336e4c77243bb3c";
+var query = "competitions/PL/standings";
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         console.log(this.responseText);
-    };
 
-    else{ 
-        
+    }
+
+    else {
         console.log("This isn't working")
+    }
 
-    };
-}
+    displayData()
+};
 xhr.open("GET", "//api.football-data.org/v2/" + query);
 xhr.setRequestHeader("X-Auth-Token", APIKEY)
 xhr.send();
+
+}
