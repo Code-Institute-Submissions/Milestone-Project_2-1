26c60807b41181ded72f2eb4a96939b8cc9b3f5c
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-03-05 15:21:31 +0000

    Added in some html structure and javascript to test displaying data on to html. Coming out as many objects, I will need to convert these to strings before they come out on to the html page

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 
-function displayData() {
+function displayDataInMyPage() {
 
-    
+    console.log(data_as_an_object);
+    console.log(data_as_an_object.standings[0].table);
+
+    var team_name = document.getElementById("team_name")
+    team_name.innerHTML = data_as_an_object.standings[0].table;
+
+
+
+
+
+
 }
 
 
@@ -16,15 +26,20 @@ var APIKEY = "b010fe05a02c4ddc8336e4c77243bb3c";
 var query = "competitions/PL/standings";
 xhr.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-        console.log(this.responseText);
+        //console.log(this.responseText);
 
+
+        the_response = xhr.responseText;
+        console.log(typeof the_response);
+
+        data_as_an_object = JSON.parse(the_response);
+
+        displayDataInMyPage (data_as_an_object);
     }
 
     else {
         console.log("This isn't working")
     }
-
-    displayData()
 };
 xhr.open("GET", "//api.football-data.org/v2/" + query);
 xhr.setRequestHeader("X-Auth-Token", APIKEY)
