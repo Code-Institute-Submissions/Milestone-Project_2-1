3658091e34dd1fe72046c0795df78649e1493248
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-03-09 11:07:03 +0000

    Deleted test files of index2.html and index2.js, where I will now try solutions in the main files themselves. Simplified structure of teamStatsInMyPage function for greater readability. Receiving txt file for roadmap guidance

--- index.js
+++ index.js
@@ -2,31 +2,29 @@ var _DATAGLOBAL = {};
 
 function getData(leagueCode, teamListId) {
 
-var xhr = new XMLHttpRequest();
-var APIKEY = "b010fe05a02c4ddc8336e4c77243bb3c";
-var query = "competitions/" + leagueCode + "/standings";
-xhr.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
+    var APIKEY = "b010fe05a02c4ddc8336e4c77243bb3c";
+    var query = "competitions/" + leagueCode + "/standings";
 
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
 
-        the_response = xhr.responseText;
-        
-        var league_data = JSON.parse(the_response);
+            the_response = xhr.responseText;            
+            var league_data = JSON.parse(the_response);
+            _DATAGLOBAL[leagueCode] = league_data;
 
-        _DATAGLOBAL[leagueCode] = league_data;
+            dropdownOptionsInMyPage(teamListId, league_data)
+            
+        }
 
-        dropdownOptionsInMyPage(teamListId, league_data)
-        
-    }
-
-    else {
-        console.log("This isn't working") // why isn't this working?
-    }
+        else {
+            console.log("This isn't working") // why isn't this working?
+        }
+    };
+    xhr.open("GET", "https://api.football-data.org/v2/" + query);
+    xhr.setRequestHeader("X-Auth-Token", APIKEY)
+    xhr.send();
 };
-xhr.open("GET", "//api.football-data.org/v2/" + query);
-xhr.setRequestHeader("X-Auth-Token", APIKEY)
-xhr.send();
-}
 
 function dropdownOptionsInMyPage(teamListId, teamListData) {
 
@@ -37,39 +35,33 @@ function dropdownOptionsInMyPage(teamListId, teamListData) {
         // console.log(table[i]["team"]["name"]);
         // team_name.innerHTML += table[i]["team"]["name"] + "<br>";
         dropdown_html_string += "<p class=\"dropdown-item\" onclick=\"teamStatsInMyPage("+i+")\">"  + table[i]["team"]["name"] + "</p>";
-        
+        // dropdown ==> onclick ==> to be replaced by event listeners so we can use "real" dropdown (jquery code)
     }
  
     // console.log(html_string);
     team_dropdown_div.innerHTML = dropdown_html_string;
+
 }
 
 getData("PL", "team-list-1");
 getData("FL1", "team-list-2");
 
-function teamStatsInMyPage(i) {
+function teamStatsInMyPage(i, team_stat_container) {
 
     // alert(_DATAGLOBAL["PL"].standings[0].table[i].team.name)
      // alert(_DATAGLOBAL["FL1"].standings[0].table[i].team.name)
 
     var team_stats_div = document.getElementById("team-stats-1");
     var stats_html_string = "";
-    var team_name = _DATAGLOBAL["PL"].standings[0].table[i].team.name;
-    var team_logo = _DATAGLOBAL["PL"].standings[0].table[i].team.crestURL;
-    var position = _DATAGLOBAL["PL"].standings[0].table[i].position;
-    var played_games = _DATAGLOBAL["PL"].standings[0].table[i].playedGames;
-    var won = _DATAGLOBAL["PL"].standings[0].table[i].won;
-    var draw = _DATAGLOBAL["PL"].standings[0].table[i].draw;
-    var lost = _DATAGLOBAL["PL"].standings[0].table[i].lost;
-    var points = _DATAGLOBAL["PL"].standings[0].table[i].points;
-    var goals_for = _DATAGLOBAL["PL"].standings[0].table[i].goalsFor;
-    var goals_against = _DATAGLOBAL["PL"].standings[0].table[i].goalsAgainst;
-    var goal_difference = _DATAGLOBAL["PL"].standings[0].table[i].goalDifference;
+    var league_table = _DATAGLOBAL["PL"].standings[0].table[i];
 
-    stats_html_string += "<p> Team Name: " + team_name + "</p>" + "<img src=\"" + team_logo + "\">" + "<p> League Position: " + position + "</p>" + 
-    "<p> Played Games: " + played_games + "</p>" + "<p> Wins: " + won + "</p>" + "<p> Draws: " + draw + "</p>" + "<p> Losses: " + lost + "</p>" +
-    "<p> Points: " + points + "</p>" + "<p> Goals For: " + goals_for + "</p>" + "<p> Goals Aginst: " + goals_against + "</p>" + "<p> Goal Difference: " + goal_difference + "</p>";
+    stats_html_string += "<p> Team Name: " +  league_table.team.name + "</p>" + "<img src=\"" + team.crestURL + "\">" + "<p> League Position: " + League_table.position + "</p>" + 
+    "<p> Played Games: " + league_table.playedGames + "</p>" + "<p> Wins: " + league_table.won + "</p>" + "<p> Draws: " + league_table.draw + "</p>" + "<p> Losses: " + league_table.lost + "</p>" +
+    "<p> Points: " + league_table.points + "</p>" + "<p> Goals For: " + league_table.goalsFor + "</p>" + "<p> Goals Against: " + league_table.goalsAgainst + "</p>" + "<p> Goal Difference: " + league_table.goalDifference + "</p>";
     
     team_stats_div.innerHTML = stats_html_string;
     // html_string += (_DATAGLOBAL["PL"].standings[0].table[i].team.name);
 };
+
+
+
