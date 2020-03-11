a4641d0985d50a9de3dc5cda80d9dc63acb0db3f
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-03-11 06:04:03 +0000

    Removed significant amount of console.log()s and commented out code that is now redundant at this stage of the project roadmap

--- assets/js/index.js
+++ assets/js/index.js
@@ -68,41 +68,18 @@ function teamMatchUp (leagueCode, teamDataId, selectedTeam) {
     teamStatsInMyPage(leagueCode, teamDataId, selectedTeam);
 }
 
-//getData("PL", "team-list-1");
-//getData("FL1", "team-list-2");
-
 function teamStatsInMyPage(leagueCode, teamDataId, selectedTeam) {
-    console.log(_DATAGLOBAL)
     var team_stats_div = document.getElementById(teamDataId);
    
     var league_table = _DATAGLOBAL[leagueCode].standings[0].table[selectedTeam];
-    console.log(league_table);
     var stats_html_string = "<p> Team Name: " + league_table.team.name + "</p>" + "<p> League Position: " + league_table.position + "</p>" +  "<p> Played Games: " + league_table.playedGames + "</p>" + "<p> Wins: " + league_table.won + "</p>" + "<p> Draws: " + league_table.draw + "</p>" + "<p> Losses: " + league_table.lost + "</p>" +
                             "<p> Points: " + league_table.points + "</p>" + "<p> Goals For: " + league_table.goalsFor + "</p>" + "<p> Goals Against: " + league_table.goalsAgainst + "</p>" + "<p> Goal Difference: " + league_table.goalDifference + "</p>";
-    
-                            console.log(stats_html_string);
 
     team_stats_div.innerHTML = stats_html_string;
 
     //"<img src=\"" + league_table.team.crestURL + "\">" need to figure out how I can convert this
 
 };
-
-
-// $(".league-list").change(function() {
-
-   // var leagueCode = $( "select.league-list option:checked" ).val();
-   // let teamNumber = $(".team-list").attr("data-team-number");
-    
-   
-
-   // console.log(teamNumber)
-    // $(this).attr('id').replace(/[^d]/g, ''); https://www.sitepoint.com/jquery-numbers-element-id/
-    // var id = $("div").attr('id').replace(/button/, '') https://stackoverflow.com/questions/2427853/jquery-get-number-from-id
-    // var leagueCode = ;
-   // var teamNumber = ;
-   // getData(leagueCode, teamNumber)
-//});
 
 function getSelectedLeague (v) {
     var myselect = document.getElementById("list-" + v);
@@ -113,18 +90,4 @@ function getSelectedLeague (v) {
 function populate(v) {
     var league = getSelectedLeague(v)
     getData(league, v);
-
-
-    // console.log(league)
-    //var i ;
-    //if(v === "1") {
-     //   i = "2";
-    //}
-    //else {
-     //   i = "1";
-   // }
-   // var myselect1 = document.getElementById("list-" + i);
-   // var league1 = myselect1.options[myselect1.selectedIndex].value;
-   // console.log(league1);
-   // getData(league1, i);
 }
