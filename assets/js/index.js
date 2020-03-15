8b9d5d55bd981b637fd384d5160b1e4751c21bb5
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-03-15 07:48:05 +0000

    Attempting to implement conditions that give user feedback regarding if the same league is chosen in both league list dropdowns. Managed so far to set up a test 'alert' plus emptying the team seletion dropdown. Logic still needs work in regards to the teamMatchUp function

--- assets/js/index.js
+++ assets/js/index.js
@@ -16,26 +16,36 @@ function getData(leagueCode, teamNumber, callback) {
             var league_data = JSON.parse(the_response);
             _DATAGLOBAL[leagueCode] = league_data;
  
-            dropDownOptionsInMyPage(teamNumber, leagueCode, league_data)
+            dropDownOptionsInMyPage(teamNumber, leagueCode, league_data);
             
         }
- 
-        else {
-            console.log("This isn't working") 
-        }
+        
         if (callback) {
             // Function's Responsibility: Callback to make other functions wait until API data is received
             callback();
         }
     };
     xhr.open("GET", "https://api.football-data.org/v2/" + query);
-    xhr.setRequestHeader("X-Auth-Token", _APIKEY)
+    xhr.setRequestHeader("X-Auth-Token", _APIKEY);
     xhr.send();
-};
+}
  
 // Function's Responsibility: To supply dropdown items in the team selection dropdown menu
 function dropDownOptionsInMyPage(teamNumber, leagueCode, teamListData) {
- 
+
+    var league_list_1 = document.getElementById("list-1");
+    var league_list_2 = document.getElementById("list-2");
+    var league_selection_1 = league_list_1.options[league_list_1.selectedIndex].value
+    var league_selection_2 = league_list_2.options[league_list_2.selectedIndex].value
+
+    if(league_selection_1 === league_selection_2) {
+        alert("Please choose another league")
+        var team_dropdown_div = document.getElementById("team-list-" + teamNumber);
+        team_dropdown_div.innerHTML = ""
+
+    }
+
+    else {
     var team_dropdown_div = document.getElementById("team-list-" + teamNumber);
     var dropdown_html_string = "";
     var table = teamListData.standings[0].table;
@@ -45,8 +55,8 @@ function dropDownOptionsInMyPage(teamNumber, leagueCode, teamListData) {
        
     }
     team_dropdown_div.innerHTML = dropdown_html_string;
- 
-};
+    }
+}
  
 // Function's Responsibility: Gets the API data and passes on to function where team stats will be displayed
 function getsDataAndSetsTeamStats (teamNumber, statsDiv, selectedTeam) {
@@ -58,7 +68,7 @@ function getsDataAndSetsTeamStats (teamNumber, statsDiv, selectedTeam) {
     }
  
     getData(selectedLeague, teamNumber, callsDataDisplay);
-};
+}
  
 // Function's Responsibility: To provide team match-up feature based on (indexed) league position
 function teamMatchUp (leagueCode, teamDataId, selectedTeam) {
@@ -69,7 +79,7 @@ function teamMatchUp (leagueCode, teamDataId, selectedTeam) {
         getsDataAndSetsTeamStats("2", "team-stats-2", selectedTeam);
     }
     teamStatsInMyPage(leagueCode, teamDataId, selectedTeam);
-};
+}
  
 // Function's Responsibility: To specify which team stats to display, followecby displaying these in HTML when function is called
 function teamStatsInMyPage(leagueCode, teamDataId, selectedTeam) {
@@ -83,23 +93,22 @@ function teamStatsInMyPage(leagueCode, teamDataId, selectedTeam) {
  
     //"<img src=\"" + league_table.team.crestURL + "\">" need to figure out how I can convert this
  
-};
- 
+}
  
 // Function's Responsibility: Getting the options values (that are league codes) plus the list numbers for later dropdown ID references
 function getSelectedLeague (v) {
-    var myselect = document.getElementById("list-" + v);
-    var league = myselect.options[myselect.selectedIndex].value;
+    var my_select = document.getElementById("list-" + v);
+    var league = my_select.options[my_select.selectedIndex].value;
     return league;
-};
+}
  
  
 // Function's Responsibility: Take stored league codes and list numbers and pass them throiugh to getData function
 function populate(v) {
     var league = getSelectedLeague(v)
     getData(league, v);
-};
- 
+}
+
 populate('1');
 populate('2');
 
