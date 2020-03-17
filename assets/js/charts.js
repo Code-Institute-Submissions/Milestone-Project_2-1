cc8daa009c78f0afebf785974248593837420849
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-03-17 16:18:35 +0000

    Created new javascript file for graph making feature, cutting out the code from index.js to the new file in regards to this feature. Also added script to html. This is to organize my javascript code better, separating an additonal feature to its own file

---
+++ assets/js/charts.js
@@ -0,0 +1,31 @@
+function teamStatsGraphs(leagueTable, teamGraphID, selectedTeam, teamNumber) {
+    if(myPieChart) {
+        myPieChart.destroy();
+        }
+    var target = "games-chart-" + teamNumber;
+    var games_chart = document.getElementById(target).getContext('2d');
+        var myPieChart = new Chart(games_chart, {
+        type: 'pie',
+        data: {
+        labels: ['Won', 'Drawn', 'Lost'],
+        datasets: [{
+            label: 'Number of Games',
+            data: [leagueTable.won, leagueTable.draw, leagueTable.lost],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)'
+            ]
+        }]
+    },
+    options: {
+        cutoutpercentage: 0,
+        responsive: true,
+        title: {
+            display: true,
+            text: '' +leagueTable.team.name+ ''
+        } 
+    }
+});
+
+}
